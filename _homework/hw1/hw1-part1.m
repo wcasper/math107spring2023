09380b115681a090376490e1f6d70c3083581d81
wcasper <williamrileycasper@gmail.com>
2023-01-29 15:13:49 -0800

    init

---
+++ _homework/hw1/hw1-part1.m
@@ -0,0 +1,20 @@
+%% Problem 1
+% define your variables a,b,c below
+
+%% Problem 2
+% define your variables x,y,z,w, and q below
+
+%% Problem 3
+
+% define variable T_room and set it to the room temperature
+
+% define variable T_0 and set it to the temperature of a living human
+
+% define a variable r and set it equal to the specific heat capacitance
+
+% define a variable T and set it equal to the temperature the body was discovered at
+
+% now use the equation above to determine the time it took for the body to cool
+% make sure to use the variable t for your result!!
+
+
---
+++ _homework/hw1/hw1-part2.m
@@ -0,0 +1,19 @@
+%% Problem 1
+% Create the row vector x
+
+% Create the row vector y
+
+% Create the matrix A
+
+
+%% Problem 2
+% Read the matrix into a matrix B.  Don't forget the semicolon!
+
+% Use rgb2gray to create a matrix B_gray which is the black and white version of B
+
+% Use size() to create a vector q whose value is the size of B
+
+% Create a variable npixels whose value is the number of pixels in B
+% Note that the number of pixels in B is the product q(1)*q(2)
+
+
